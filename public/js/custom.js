86eb28ce6d096522e74d7d2d02bcbd11afbeca69
aldiui <aldijaya280902@gmail.com>
2024-01-14 19:40:13 +0700

    integrasi cors

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,6 +6,10 @@ const datatableCall = (targetId, url, columns) => {
             type: "GET",
         },
         columns: columns,
+        lengthMenu: [
+            [25, 50, 100, 250, -1],
+            [25, 50, 100, 250, "All"],
+        ],
     });
 };
 
