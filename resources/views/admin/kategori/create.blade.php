7c15824f33b2b31eff4eb67bdc0b639bfa8ba279
aldiui <aldijaya280902@gmail.com>
2024-01-15 10:34:58 +0700

    update tampilan modal centered

--- resources/views/admin/kategori/create.blade.php
+++ resources/views/admin/kategori/create.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" role="dialog" id="createModal">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Tambah Data</h5>
--- resources/views/admin/kategori/edit.blade.php
+++ resources/views/admin/kategori/edit.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" role="dialog" id="editModal">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Ubah Data</h5>
