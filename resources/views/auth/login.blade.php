f06630f2d1f711d29e0b60dcd97f8deaa1c572e5
aldiui <aldijaya280902@gmail.com>
2024-01-15 15:46:12 +0700

    update mode json

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,7 +60,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-8 col-12 order-lg-2 min-vh-100 background-walk-y position-relative overlay-gradient-bottom order-1"
+                <div class="d-none d-lg-block col-lg-8 col-12 order-lg-2 min-vh-100 background-walk-y position-relative overlay-gradient-bottom order-1"
                     data-background="{{ asset('img/unsplash/login-bg.jpg') }}">
                     <div class="absolute-bottom-left index-2">
                         <div class="text-light p-5 pb-2">
