e369e8d05f0c4e3f3d87b4bce7c7b84bd0669f55
aldiui <aldijaya280902@gmail.com>
2024-01-21 10:24:15 +0700

    l

--- app/Http/Controllers/Admin/PresensiController.php
+++ app/Http/Controllers/Admin/PresensiController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Traits\ApiResponder;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class PresensiController extends Controller
 {
-    //
+    use ApiResponder;
+    
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $presensis = Presensi::all();
+            if($request->input("mode") == "datatable"){
+                return DataTables::of($presensis)->make(true);
+            }
+
+            return $this->successResponse($presensis, 'Data Presensi ditemukan.'); 
+        }
+    
+        return view('admin.presensi.index');
+    }
 }
