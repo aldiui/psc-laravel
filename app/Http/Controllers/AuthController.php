5e0e97c2a36f18d950bcd9e1aab1976874c1b419
aldiui <aldijaya280902@gmail.com>
2024-05-19 10:08:21 +0700

    updated data extra

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
             }
 
             if (!Auth::attempt($request->only('email', 'password'))) {
-                return $this->errorResponse(null, 'Email atau password tidak valid.', 401);
+                return $this->errorResponse(null, 'Password tidak valid.', 401);
             }
 
             $user = Auth::user();
