29a528cd83ba9db52d6e34e4c4d3f4d9366a344d
aldiui <aldijaya280902@gmail.com>
2024-05-19 11:03:17 +0700

    updated data extra

--- database/migrations/2024_01_10_140841_create_kategoris_table.php
+++ database/migrations/2024_01_10_140841_create_kategoris_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('kategoris', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->text('deskripsi');
+            $table->text('deskripsi')->nullable();
             $table->timestamps();
         });
     }
